3c13b9150dcefe8d61cecfe49b3585c5abee29b5
Samuel Francis <samuel.francis@learner.manipal.edu>
2023-02-19 22:04:58 +0530

    Optimized JS using debounced event listener

--- main.js
+++ main.js
@@ -1,27 +1,40 @@
-// manipulate pre
+// Cache pre element reference
 const pre = document.querySelector("pre");
 
-// add event listener to track mouse move
-pre.addEventListener("mousemove", (e) => {
-    // call rotateElement function
-    rotateElement(e, pre);
-});
+// Debounce mousemove events
+function debounce(func, wait) {
+  let timeout;
+  return function executedFunction(...args) {
+    const later = () => {
+      clearTimeout(timeout);
+      func(...args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+}
 
-// function to rotate element
+// Add debounced event listener to track mouse move
+pre.addEventListener("mousemove", debounce((e) => {
+    // Call rotateElement function
+    rotateElement(e, pre);
+}, 10));
+
+// Function to rotate element
 function rotateElement(event, element) { 
-    // get mouse position
+    // Get mouse position
     const x = event.clientX;
     const y = event.clientY;
 
-    //find middle of the page
+    // Find middle of the page
     const middleX = window.innerWidth / 2;
     const middleY = window.innerHeight / 2;
 
-    //get offset from the middle
-    const offsetX = ((x - middleX)/ middleX * 100);
-    const offsetY = ((y - middleY)/ middleY * 100);
-    // console.log(offsetX, offsetY);
+    // Get offset from the middle
+    const offsetX = ((x - middleX) / middleX * 100);
+    const offsetY = ((y - middleY) / middleY * 100);
 
+    // Use CSS variables to set rotation
     element.style.setProperty("--rotateX", -1 * offsetY + "deg");
     element.style.setProperty("--rotateY", -1 * offsetX + "deg");
 }
